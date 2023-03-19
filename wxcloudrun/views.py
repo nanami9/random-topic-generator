cc935864a408985ee89ec338391dc7ffed6af616
nanami9 <77765902+nanami9@users.noreply.github.com>
2023-03-19 13:09:25 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,7 +4,7 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-from lib import clueai
+from ..lib import clueai
 
 
 @app.route('/')
