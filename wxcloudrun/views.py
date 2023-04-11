5019d2a938b1bf0412a214b49088591dfc13ee8f
nanami9 <77765902+nanami9@users.noreply.github.com>
2023-04-11 09:20:22 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,11 +4,8 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-import clueai
 import requests
 import json
-import asyncio
-import aiohttp
 
 @app.route('/')
 def index():
