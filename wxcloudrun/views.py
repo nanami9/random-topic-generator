5e4bcce61fb3fde750b8dd8a59753be51d576628
nanami9 <77765902+nanami9@users.noreply.github.com>
2023-04-12 11:45:00 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -10,7 +10,11 @@ import aiohttp
 import asyncio
 from celery import Celery
 
-celery = Celery(app.name, broker='redis://localhost:6379/0')
+
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 
