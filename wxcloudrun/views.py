00827a23630d541bc7b3e0441717786ae18e13ed
nanami9 <77765902+nanami9@users.noreply.github.com>
2023-04-12 18:49:55 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -45,6 +45,26 @@ def gpt2():
         print('Error:', response.status_code)
         
     return make_succ_response(task_id)
+
+@app.route('/api/gpt2/status', methods=['POST'])
+def gpt2_status():
+    # 获取发送来的id
+    data = request.get_json()
+    task_id = data["task_id"]
+    # 构造请求URL
+    url = f'{api_endpoint}?task_id={task_id}'
+    # 发送GET请求并获取响应
+    response = requests.get(url)
+    # 解析JSON响应
+    if response.ok:
+        response_data = response.json()
+        if 'error' in response_data:
+            make_succ_response("failed")
+        else:
+            task_result = response_data
+            make_succ_response(task_result)
+    else:
+        make_succ_response("Failed to query task status.")
     
     
     
